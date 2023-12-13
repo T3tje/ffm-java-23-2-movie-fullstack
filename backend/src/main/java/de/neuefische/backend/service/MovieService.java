0f6798c57dfd11aae308dfc9eb8e2e27fbea90b4
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-12-13 11:24:03 +0100

    add Constructor, Service annotations for Service

--- backend/src/main/java/de/neuefische/backend/service/MovieService.java
+++ backend/src/main/java/de/neuefische/backend/service/MovieService.java
@@ -1,4 +1,11 @@
 package de.neuefische.backend.service;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class MovieService {
 }
