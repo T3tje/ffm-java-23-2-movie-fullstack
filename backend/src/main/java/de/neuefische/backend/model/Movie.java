78fb32c237e96fe3ba36447de2940e2f284456f3
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-12-13 13:09:00 +0100

    change Movie DataType

--- backend/src/main/java/de/neuefische/backend/model/Movie.java
+++ backend/src/main/java/de/neuefische/backend/model/Movie.java
@@ -1,10 +1,8 @@
 package de.neuefische.backend.model;
 //Titles Objekt von der API/Datenbank
 public record Movie(String id,
-                    Image primaryImage,
                     MovieType titleType,
                     TitleText titleText,
                     TitleText originalTitleText,
-                    ReleaseYear releaseYear,
-                    String releaseDate) {
+                    ReleaseYear releaseYear) {
 }
