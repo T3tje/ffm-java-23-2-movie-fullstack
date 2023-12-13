15e27b0c9269c3f476a6d8d7cc996889c210584b
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-12-13 12:00:40 +0100

    add MovieController

--- backend/src/main/java/de/neuefische/backend/controller/MovieController.java
+++ backend/src/main/java/de/neuefische/backend/controller/MovieController.java
@@ -1,11 +1,19 @@
 package de.neuefische.backend.controller;
 
+import de.neuefische.backend.model.MovieDTO;
+import de.neuefische.backend.service.MovieService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/titles")
+@RequestMapping("/api/titles")
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class MovieController {
 
     private final MovieService service;
