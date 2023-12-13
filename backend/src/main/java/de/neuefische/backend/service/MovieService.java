8f3482a45b65be16c063e5d05cc3a525619e59f5
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-12-13 12:00:27 +0100

    add Image

--- backend/src/main/java/de/neuefische/backend/service/MovieService.java
+++ backend/src/main/java/de/neuefische/backend/service/MovieService.java
@@ -1,11 +1,23 @@
 package de.neuefische.backend.service;
 
+import de.neuefische.backend.model.Movie;
+import de.neuefische.backend.model.MovieDTO;
+import de.neuefische.backend.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class MovieService {
+
+    private final MovieRepository repo;
+    private final IdService idService;
+
+    public List<MovieDTO> getAllMovies(){
+        return repo.getAllMovies();
+    }
 }
