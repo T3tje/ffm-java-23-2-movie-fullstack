22601639cfc7b50946ba2b0e6951d5a43e9b4520
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-12-13 13:10:54 +0100

    change RequestMapping

--- backend/src/main/java/de/neuefische/backend/controller/MovieController.java
+++ backend/src/main/java/de/neuefische/backend/controller/MovieController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/titles")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class MovieController {
 
