5e5f30207bb1d2769bb05604a4644fd786c8851c
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-12-19 15:37:00 +0100

    add url (for Images) to MovieSortDTO.java

--- backend/src/main/java/de/neuefische/backend/model/MovieSortDTO.java
+++ backend/src/main/java/de/neuefische/backend/model/MovieSortDTO.java
@@ -1,9 +1,12 @@
 package de.neuefische.backend.model;
 
+import org.apache.logging.log4j.util.PropertySource;
+
 public record MovieSortDTO(
         String id,
         String title,
         double rating,
-        int releaseYear
+        int releaseYear,
+        String url
 ) {
 }
